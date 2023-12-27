e2e5510415958de8690b155b8c5286762dd0a529
DeanNguyen98 <nctuan.hn@gmail.com>
2023-12-27 22:55:44 +1100

    Changing the API from http to https

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -2,7 +2,7 @@ import { appendtoDOm } from "./DOM";
 
 async function getWeatherData (city) {
     try {
-        const weatherapi = `http://api.weatherapi.com/v1/current.json?key=2c9b3c2d78284b99bb6194727230712&q=${city}&aqi=no`
+        const weatherapi = `https://api.weatherapi.com/v1/current.json?key=2c9b3c2d78284b99bb6194727230712&q=${city}&aqi=no`
         const response = await fetch(weatherapi, {mode: "cors"});
         const weatherdata = await response.json();
         if (!response.ok) {
@@ -16,3 +16,4 @@ async function getWeatherData (city) {
 }
 
 export {getWeatherData};
+
